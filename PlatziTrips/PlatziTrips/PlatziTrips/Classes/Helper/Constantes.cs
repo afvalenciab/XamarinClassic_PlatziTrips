d2ee88a27a312511f739df783c621659b78346bd
Andres Felipe Valencia Benitez <anfevale@bancolombia.com.co>
2017-10-18 23:49:24 -0500

    Se crea la calse constantes para alamcenar URL y vairables necesarias para consumir la API de Foursquare

---
+++ PlatziTrips/PlatziTrips/PlatziTrips/Classes/Helper/Constantes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlatziTrips.Classes.Helper
+{
+    public class Constantes
+    {
+        public const string CLIENT_ID = "M3NUDF5KAYHN5ZAFEPPNGID0TALSD1U4B1KFNXYOFJYQA22E";
+        public const string CLIENT_SECRET = "MQSMMNIHXPQ2MXA2JDUBAAOOBCCENKNPUWAEYF4QB2IWQO0Q";
+
+        public static string ObtenerUrlCategorias()
+        {
+            return $"https://api.foursquare.com/v2/venues/categories?client_id={CLIENT_ID}&client_secret={CLIENT_SECRET}&v={DateTime.Now.ToString("yyyyMMdd")}";
+        }
+
+    }
+}
